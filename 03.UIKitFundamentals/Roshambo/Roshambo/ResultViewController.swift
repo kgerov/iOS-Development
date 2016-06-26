6154c47de262988e2000ca47da406f470eee01e1
kgerov@abv.bg <kgerov@abv.bg>
2016-06-26 18:39:33 +0300

    Finish ResultViewController

--- 03.UIKitFundamentals/Roshambo/Roshambo/ResultViewController.swift
+++ 03.UIKitFundamentals/Roshambo/Roshambo/ResultViewController.swift
@@ -13,10 +13,72 @@ class ResultViewController: UIViewController {
     var userChoice: Roshambo?
     var computerChoice: Roshambo?
     
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    enum gameOutcome: Int { case UserWon = 0, ComputerWon, Tie}
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(userChoice)
-        print(computerChoice)
+        
+        if userChoice == computerChoice {
+            displayGameOutcome(gameOutcome.Tie, winner: nil)
+            return
+        }
+        
+        switch userChoice! {
+        case Roshambo.Rock:
+            if computerChoice == Roshambo.Scissor {
+                displayGameOutcome(.UserWon, winner: .Rock)
+            } else if computerChoice == Roshambo.Paper {
+                displayGameOutcome(.ComputerWon, winner: .Paper)
+            }
+        case Roshambo.Paper:
+            if computerChoice == Roshambo.Scissor {
+                displayGameOutcome(.ComputerWon, winner: .Scissor)
+            } else if computerChoice == Roshambo.Rock {
+                displayGameOutcome(.UserWon, winner: .Paper)
+            }
+        case Roshambo.Scissor:
+            if computerChoice == Roshambo.Paper {
+                displayGameOutcome(.UserWon, winner: .Scissor)
+            } else if computerChoice == Roshambo.Rock {
+                displayGameOutcome(.ComputerWon, winner: .Rock)
+            }
+        }
+    }
+    
+    private func displayGameOutcome(outcome: gameOutcome, winner: Roshambo?) {
+        var resultText = ""
+        
+        switch winner {
+        case .Some(Roshambo.Rock):
+            resultText = "Rock crushes scissors. "
+            resultImage.image = UIImage(named: "RockCrushesScissors")
+        case .Some(Roshambo.Paper):
+            resultText = "Paper covers rock. "
+            resultImage.image = UIImage(named: "PaperCoversRock")
+        case .Some(Roshambo.Scissor):
+            resultText = "Scissors cut paper. "
+            resultImage.image = UIImage(named: "ScissorsCutPaper")
+        default:
+            resultImage.image = UIImage(named: "Tie")
+        }
+        
+        switch outcome {
+        case gameOutcome.ComputerWon:
+            resultText += "You lose!"
+        case gameOutcome.UserWon:
+            resultText += "You win!"
+        case gameOutcome.Tie:
+            resultText += "It's a tie!"
+        }
+        
+        self.resultLabel.text = resultText;
+    }
+    
+    @IBAction func closeView() {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 }
