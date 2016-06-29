c808df46462469286bcdad40fbb2ae67f512ba30
kgerov@abv.bg <kgerov@abv.bg>
2016-06-29 23:21:11 +0300

    Create project TextFields

---
+++ 03.UIKitFundamentals/03.TheDelegatePatern/TextFields/TextFieldsTests/TextFieldsTests.swift
@@ -0,0 +1,36 @@
+//
+//  TextFieldsTests.swift
+//  TextFieldsTests
+//
+//  Created by Konstantin Gerov on 6/29/16.
+//  Copyright © 2016 Konstantin Gerov. All rights reserved.
+//
+
+import XCTest
+@testable import TextFields
+
+class TextFieldsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
