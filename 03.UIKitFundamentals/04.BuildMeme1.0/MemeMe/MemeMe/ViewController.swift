50f27c96ea1247f91400378f5a0f83e4429644d3
kgerov@abv.bg <kgerov@abv.bg>
2016-07-04 15:02:08 +0300

    Connect UI to ViewController

--- 03.UIKitFundamentals/04.BuildMeme1.0/MemeMe/MemeMe/ViewController.swift
+++ 03.UIKitFundamentals/04.BuildMeme1.0/MemeMe/MemeMe/ViewController.swift
@@ -8,14 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+    
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName : UIColor.blackColor(),
+        NSForegroundColorAttributeName : UIColor.whiteColor(),
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName : -7
+    ]
+    
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.textAlignment = NSTextAlignment.Center
+        //topTextField.delegate = memeDelegate
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.textAlignment = NSTextAlignment.Center
+        //bottomTextField.delegate = memeDelegate
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    @IBAction func selectImageFromAlbum(sender: AnyObject) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func selectImageFromCamera(sender: AnyObject) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = UIImagePickerControllerSourceType.Camera
+        self.presentViewController(pickerController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func shareMeme(sender: AnyObject) {
+    }
+    
+    @IBAction func cancelMemeEditing(sender: AnyObject) {
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imageView.image = image
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
 
