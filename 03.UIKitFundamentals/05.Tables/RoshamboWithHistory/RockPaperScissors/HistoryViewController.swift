a3066d9799612366ede710f09b9f009800578f50
kgerov@abv.bg <kgerov@abv.bg>
2016-07-20 15:45:42 +0300

    Add storyboard file + view controller class for History

---
+++ 03.UIKitFundamentals/05.Tables/RoshamboWithHistory/RockPaperScissors/HistoryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HistoryViewController.swift
+//  RockPaperScissors
+//
+//  Created by Konstantin Gerov on 7/20/16.
+//  Copyright © 2016 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
