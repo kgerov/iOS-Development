c25c0d0aaebfe58895a16c8d4c1db724303ea28f
kgerov@abv.bg <kgerov@abv.bg>
2016-07-21 13:29:52 +0300

    Add start over button

--- 03.UIKitFundamentals/06.Navigation/MYOA/MYOA/ViewController.swift
+++ 03.UIKitFundamentals/06.Navigation/MYOA/MYOA/ViewController.swift
@@ -12,14 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem (
+            title: "Start Over",
+            style: UIBarButtonItemStyle.Plain,
+            target: self,
+            action: #selector(ViewController.startOver))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func startOver() {
+        if let navigationController = self.navigationController {
+            navigationController.popToRootViewControllerAnimated(true)
+        }
     }
-
-
 }
 
