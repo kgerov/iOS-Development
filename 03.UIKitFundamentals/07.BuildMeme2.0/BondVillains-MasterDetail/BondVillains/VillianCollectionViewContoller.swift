ada1f216b0a3cafea3deebf177a4c5cb1b8bc41e
kgerov@abv.bg <kgerov@abv.bg>
2016-07-21 18:15:54 +0300

    Add Collection View Contoller to Storyboard

---
+++ 03.UIKitFundamentals/07.BuildMeme2.0/BondVillains-MasterDetail/BondVillains/VillianCollectionViewContoller.swift
@@ -0,0 +1,14 @@
+//
+//  VillianCollectionViewContoller.swift
+//  BondVillains
+//
+//  Created by Konstantin Gerov on 7/21/16.
+//  Copyright © 2016 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class VillianCollectionViewContoller : UICollectionViewController {
+
+    
+}
