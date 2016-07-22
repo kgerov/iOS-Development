e3f036f87caa7472b1abf8ce0c77575569fe7b18
kgerov@abv.bg <kgerov@abv.bg>
2016-07-22 23:28:09 +0300

    Add custom cells

---
+++ 03.UIKitFundamentals/07.BuildMeme2.0/MemeMe/MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Konstantin Gerov on 7/22/16.
+//  Copyright © 2016 Konstantin Gerov. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell : UICollectionViewCell {
+
+}
---
+++ 03.UIKitFundamentals/07.BuildMeme2.0/MemeMe/MemeMe/MemeTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMe
+//
+//  Created by Konstantin Gerov on 7/22/16.
+//  Copyright © 2016 Konstantin Gerov. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewCell : UITableViewCell {
+
+    @IBOutlet weak var memeImage: UIImageView!
+}
