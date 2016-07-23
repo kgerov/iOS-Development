7e372560d87f7d046d2f8359708db1def5de09a4
kgerov@abv.bg <kgerov@abv.bg>
2016-07-23 14:20:30 +0300

    Add Constants

--- 03.UIKitFundamentals/07.BuildMeme2.0/MemeMe/Constants.swift
+++ 03.UIKitFundamentals/07.BuildMeme2.0/MemeMe/Constants.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2016 Konstantin Gerov. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct Constants {
+    struct Storyboard {
+        static let memeTextAttributes = [
+            NSStrokeColorAttributeName : UIColor.blackColor(),
+            NSForegroundColorAttributeName : UIColor.whiteColor(),
+            NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName : -3.3
+        ]
+        
+        static let memeLabelAttributes = [
+            NSStrokeColorAttributeName : UIColor.blackColor(),
+            NSForegroundColorAttributeName : UIColor.whiteColor(),
+            NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 15)!,
+            NSStrokeWidthAttributeName : -3.3
+        ]
+    }
+}
