a12c46bad39f3c4ec113cf4c7a3c48a76469afcc
kgerov@abv.bg <kgerov@abv.bg>
2016-07-25 23:24:52 +0300

    add playground file for closures

---
+++ 04.Networking-in-iOS/00.GrandCentralDispatch/01.Closures/Closures.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var sum = {(x: Int, y: Int) -> Int
+    in
+    return x + y
+}
+
+
+var sumFloat = {(x: Float, y: Float) -> Float
+    in
+    return x + y
+}
+
+var arr = [sum]
