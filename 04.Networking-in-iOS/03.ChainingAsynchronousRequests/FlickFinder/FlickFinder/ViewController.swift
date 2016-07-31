157c9ef4e3792d5f3c8588bdf8f6aa093e2196ca
kgerov@abv.bg <kgerov@abv.bg>
2016-08-01 00:43:14 +0300

    Finish search method

--- 04.Networking-in-iOS/03.ChainingAsynchronousRequests/FlickFinder/FlickFinder/ViewController.swift
+++ 04.Networking-in-iOS/03.ChainingAsynchronousRequests/FlickFinder/FlickFinder/ViewController.swift
@@ -54,7 +54,7 @@ class ViewController: UIViewController {
         
         if !phraseTextField.text!.isEmpty {
             photoTitleLabel.text = "Searching..."
-            // TODO: Set necessary parameters!
+            
             let methodParameters: [String: String!] =
                 [Constants.FlickrParameterKeys.SafeSearch: Constants.FlickrParameterValues.UseSafeSearch,
                  Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
@@ -78,7 +78,7 @@ class ViewController: UIViewController {
         
         if isTextFieldValid(latitudeTextField, forRange: Constants.Flickr.SearchLatRange) && isTextFieldValid(longitudeTextField, forRange: Constants.Flickr.SearchLonRange) {
             photoTitleLabel.text = "Searching..."
-            // TODO: Set necessary parameters!
+            
             let methodParameters: [String: String!] =
                 [Constants.FlickrParameterKeys.SafeSearch: Constants.FlickrParameterValues.UseSafeSearch,
                  Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
@@ -124,10 +124,73 @@ class ViewController: UIViewController {
                     self.photoImageView.image = nil
                 }
             }
-            if error == nil {
-                print(data)
-            } else {
-                print("Error")
+            
+            guard (error == nil) else {
+                displayError("The request did not succeed: \(error)")
+                return
+            }
+            
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode
+                where statusCode >= 200 && statusCode <= 299 else {
+                
+                displayError("Your request returned a status code different than 2xx")
+                    return
+            }
+            
+            guard let data = data else {
+                displayError("No data was returned by your request")
+                return
+            }
+            
+            let parsedResult: AnyObject!
+            
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                displayError("Could not parse the JSON data: \(data)")
+                return
+            }
+            
+            guard let stat = parsedResult[Constants.FlickrResponseKeys.Status] as? String
+                where stat == Constants.FlickrResponseValues.OKStatus else {
+                    
+                displayError("Flickr API return an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            guard let photos = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String: AnyObject],
+                let photoArray = photos[Constants.FlickrResponseKeys.Photo] as? [[String: AnyObject]] else {
+                
+                displayError("Cannont find keys '\(Constants.FlickrResponseKeys.Photo)' and '\(Constants.FlickrResponseKeys.Photos)' in \(parsedResult)")
+                return
+            }
+            
+            guard (photoArray.count != 0) else {
+                displayError("No photos found. Search again.")
+                return
+            }
+            
+            let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+            let photo = photoArray[randomPhotoIndex] as [String: AnyObject]
+            
+            guard let photoUrl = photo[Constants.FlickrResponseKeys.MediumURL] as? String,
+                let photoTitle = photo[Constants.FlickrResponseKeys.Title] as? String else {
+                
+                displayError("Cannot find key '\(Constants.FlickrResponseKeys.MediumURL)' in \(photo)")
+                return
+            }
+            
+            let imageUrl = NSURL(string: photoUrl)
+            
+            guard let imageData = NSData(contentsOfURL: imageUrl!) else {
+                displayError("Could not download photo from url: \(photoUrl)")
+                return
+            }
+            
+            performUIUpdatesOnMain {
+                self.photoImageView.image = UIImage(data: imageData)
+                self.photoTitleLabel.text = photoTitle
+                self.setUIEnabled(true)
             }
         }
         
