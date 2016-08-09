1d0961052be2232aa12b9f006119862c48b427b2
kgerov@abv.bg <kgerov@abv.bg>
2016-08-09 20:45:39 +0300

    Add file with constants for clients

---
+++ 04.Networking-in-iOS/06.OnTheMap/OnTheMap/OnTheMap/ClientsConstants.swift
@@ -0,0 +1,28 @@
+//
+//  ClientsConstants.swift
+//  OnTheMap
+//
+//  Created by Konstantin Gerov on 8/9/16.
+//  Copyright © 2016 Konstantin Gerov. All rights reserved.
+//
+
+extension BaseClient {
+
+    struct Udacity {
+        
+        struct Constants {
+            static let ApiScheme = "https"
+            static let ApiHost = "api.themoviedb.org"
+            static let ApiPath = "/api"
+        }
+    }
+    
+    struct Parse {
+        
+        struct Constants {
+            static let ApiScheme = "https"
+            static let ApiHost = "parse.udacity.com"
+            static let ApiPath = "/parse"
+        }
+    }
+}
