78d82980ce1634a631acf3b1fa8b733b76110ceb
kgerov@abv.bg <kgerov@abv.bg>
2016-08-21 15:16:33 +0200

    Add view for displaying location's collection

---
+++ 05.Persistence-and-CoreData/04.VirtualTourist/VirtualTourist/VirtualTourist/LocationCollectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LocationCollectionViewController.swift
+//  VirtualTourist
+//
+//  Created by Konstantin Gerov on 8/21/16.
+//  Copyright © 2016 Konstantin Gerov. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationCollectionViewController : UIViewController {
+    
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func getNewCollection(sender: AnyObject) {
+        
+    }
+}
---
+++ 05.Persistence-and-CoreData/04.VirtualTourist/VirtualTourist/VirtualTourist/PhotoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Konstantin Gerov on 8/21/16.
+//  Copyright © 2016 Konstantin Gerov. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell : UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
