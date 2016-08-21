bdd6af0e2c834f34ddd09aed4f7ad068a832607f
kgerov@abv.bg <kgerov@abv.bg>
2016-08-21 13:59:42 +0200

    Add map view controller

---
+++ 05.Persistence-and-CoreData/04.VirtualTourist/VirtualTourist/VirtualTourist/MapViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by Konstantin Gerov on 8/21/16.
+//  Copyright © 2016 Konstantin Gerov. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController : UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func removeButtonPressed(sender: AnyObject) {
+    
+    }
+    
+    // MARK: - MKMapViewDelegate
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinColor = .Red
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    
+    // This delegate method is implemented to respond to taps. It opens the system browser
+    // to the URL specified in the annotationViews subtitle property.
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        print("Open location's album")
+    }
+}
