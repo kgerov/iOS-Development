41ffd6fd4e5543642625a3235002edc30b80dcfa
kgerov@abv.bg <kgerov@abv.bg>
2016-08-21 14:44:20 +0200

    Add model class

--- 05.Persistence-and-CoreData/04.VirtualTourist/VirtualTourist/VirtualTourist/AppDelegate.swift
+++ 05.Persistence-and-CoreData/04.VirtualTourist/VirtualTourist/VirtualTourist/AppDelegate.swift
@@ -13,9 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    let stack = CoreDataStack(modelName: "Model")!
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Autosave
+        stack.autoSave(60)
+        
         return true
     }
 
