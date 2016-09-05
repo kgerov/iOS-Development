ea9af5350e074f9566363ce5259ad918772c3d5b
kgerov@abv.bg <kgerov@abv.bg>
2016-09-04 18:33:53 -0700

    Add UI for login screen

---
+++ 06.FinalProject/Diety/Diety/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  Diety
+//
+//  Created by Konstantin Gerov on 9/4/16.
+//  Copyright © 2016 Konstantin Gerov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: LoginTextField!
+    @IBOutlet weak var passwordTextField: LoginTextField!
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    @IBAction func loginButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func signupButtonPressed(sender: AnyObject) {
+    }
+}
