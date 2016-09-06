3873cf1c187a64d770cb6c8ffe8fe92fdfe96a5b
kgerov@abv.bg <kgerov@abv.bg>
2016-09-05 18:03:25 -0700

    Add table view controller for meals + dashboard image

---
+++ 06.FinalProject/Diety/Diety/AppConstants.swift
@@ -0,0 +1,18 @@
+//
+//  AppConstants.swift
+//  Diety
+//
+//  Created by Konstantin Gerov on 9/5/16.
+//  Copyright © 2016 Konstantin Gerov. All rights reserved.
+//
+
+struct AppConstants {
+    
+    struct MealTypes {
+        static let Breakfast = "Breakfast"
+        static let Brunch = "Brunch"
+        static let Lunch = "Lunch"
+        static let Snack = "Afternoon Snack"
+        static let Dinner = "Dinner"
+    }
+}
--- 06.FinalProject/Diety/Diety/MealsTableViewController.swift
+++ 06.FinalProject/Diety/Diety/MealsTableViewController.swift
@@ -10,7 +10,30 @@ import UIKit
 
 class MealsTableViewController : UITableViewController {
     
+    let mealTypes = [AppConstants.MealTypes.Breakfast, AppConstants.MealTypes.Brunch,
+                     AppConstants.MealTypes.Lunch, AppConstants.MealTypes.Snack,
+                     AppConstants.MealTypes.Dinner]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.mealTypes.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("mealTypeCell")! as UITableViewCell
+        let mealType = self.mealTypes[indexPath.row]
+        
+        cell.textLabel?.text = mealType
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let mealType = self.mealTypes[indexPath.row]
+        print(mealType)
     }
 }
